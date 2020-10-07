cacf49509a0e4cdbaad12ccea2b062f8077da21f
Gabriel Ogbonnaya <ogbonnaya83512@gmail.com>
2020-10-06 23:25:00 -0700

    added removal of tasks, formatted task table to overflow-y scroll

--- src/App.js
+++ src/App.js
@@ -15,8 +15,16 @@ class App extends React.Component {
     this.onChangeHandlerTextBox = this.onChangeHandlerTextBox.bind(this);
     this.onClickHandlerButton = this.onClickHandlerButton.bind(this);
   }
+  onClickHandlerDeleteTask = (id) => {
+    let task_copy = [...this.state.tasks];
+    task_copy.splice(id, 1);
+    this.setState({
+      tasks: task_copy,
+    });
+  };
 
   onClickHandlerButton = (e) => {
+    console.log(e);
     let curr = this.state.current_input;
     let tasks = [...this.state.tasks];
     tasks.push(curr);
@@ -35,11 +43,26 @@ class App extends React.Component {
 
   render() {
     const tasks = this.state.tasks.map((value, index) => (
-      <Bubble key={index} task={value} />
+      <Bubble
+        key={index}
+        id={index}
+        task={value}
+        deleteTask={this.onClickHandlerDeleteTask}
+      />
     ));
+    const no_tasks = (
+      <span className={styles.no_tasks_content}>
+        <p className={styles.no_tasks}>No Tasks.</p>
+        <p className={styles.no_tasks_sub}>Lets Add Some!</p>
+      </span>
+    );
+
     return (
       <div className={styles.main}>
-        <div>{tasks}</div>
+        <div className={styles.taskScroll}>
+          {this.state.tasks.length ? tasks : no_tasks}
+          <div className={styles.fade}></div>
+        </div>
         <Button onClickHandler={this.onClickHandlerButton} />
         <Input
           onChangeHandler={this.onChangeHandlerTextBox}
--- src/components/bubble.jsx
+++ src/components/bubble.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import styles from "./bubble.module.css";
-const Bubble = ({ task }) => {
+const Bubble = ({ task, deleteTask, id }) => {
   return (
     <div
       className={`${styles.task} ${styles.green} ${styles.animated} ${styles.fadeInUp}`}
+      onClick={() => deleteTask(id)}
     >
       <div className={`${styles.inner}`}>
         <h3>New Homepage Design</h3>
         <h4>{task}</h4>
         <div className={`${styles.task_infos}`}>
           <p className="date">
-            <i className="far fa-calendar" /> 8.20.19
+            <i className="far fa-calendar" /> 10/6/2020
           </p>
           <p className="time">
             <i className="far fa-clock" /> 0/3
